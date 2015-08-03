fd5f16ce18dc8a9900c850715c99b1aa5bc1a10b
PhompAng <tingtong003tomy@gmail.com>
2015-08-03 15:26:11 +0700

    Fix: Station Map always show "BCEN";
    My Code works but I don't know why.

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,8 +9,8 @@ app.controller(
             method: "POST",
             url: "http://127.0.0.1:8000/calculate",
             data: {
-                    origin: "A3",
-                    destination: "BS12",
+                    origin: "A1",
+                    destination: "BE2",
                     card_type_bts: "0",
                     card_type_mrt: "0",
                     card_type_arl: "0"
@@ -31,6 +31,7 @@ app.controller(
             bts_cnt = 0;
             mrt_cnt = 0;
             station_cnt[data["route"][0]] = 0;
+            $scope.same_line = false;
             var tmp = data["route"][0];
 
             $.each(data["route"], function(i, val) {
@@ -45,11 +46,25 @@ app.controller(
                 if (typeof data["route"][i+1] == "undefined") {
                     return;
                 };
-
+                console.log(data["route"][i]);
                 if (data["route"][i+1] === "BCEN") {
-                    station_cnt[tmp] += 1;
-                    station_cnt["BCEN"] = 0;
-                    tmp = data["route"][i+1];
+                    if (typeof data["route"][i+2] == "undefined") {
+                        return;
+                    };
+                    if (data["route"][i][1] == "N" && data["route"][i+2][1] == "E") {
+                        station_cnt[tmp] += 1;
+                    } else if (data["route"][i][1] == "E" && data["route"][i+2][1] == "N") {
+                        station_cnt[tmp] += 1;
+                    } else if (data["route"][i][1] == "W" && data["route"][i+2][1] == "S") {
+                        station_cnt[tmp] += 1;
+                    } else if (data["route"][i][1] == "S" && data["route"][i+2][1] == "W") {
+                        station_cnt[tmp] += 1;
+                    } else {
+                        station_cnt[tmp] += 1;
+                        station_cnt["BCEN"] = 0;
+                        tmp = data["route"][i+1];
+                        $scope.same_line = true;
+                    }
                 } else if (data["route"][i][0] === data["route"][i+1][0]) {
                     station_cnt[tmp] += 1;
                 } else {
