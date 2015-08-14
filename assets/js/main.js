1fa5ae3ae320ed03e5630a1f108d8fddabe46819
PhompAng <tingtong003tomy@gmail.com>
2015-08-14 13:04:05 +0700

    clean up (2/2)

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,70 +18,23 @@ app.controller(
         };
         var res = $http(aaa);
         res.success(function(data, status, headers, config) {
-            console.log(data);
-            $scope.stations = data["route"];
-            $scope.station_name = station_name;
-
-            $scope.object_route = data["object_route"];
-
             $scope.response = data;
-            $scope.origin = data["origin"];
-            $scope.destination = data["destination"];
-
-            var station_cnt = {};
-            arl_cnt = 0;
-            bts_cnt = 0;
-            mrt_cnt = 0;
-            station_cnt[data["route"][0]] = 0;
+            $scope.object_route = data["object_route"];
             $scope.bts_same_line = data["BTS_same_line"];
-            $scope.same_line = false;
-            var tmp = data["route"][0];
+
+            $scope.arl_cnt = 0;
+            $scope.bts_cnt = 0;
+            $scope.mrt_cnt = 0;
 
             $.each(data["route"], function(i, val) {
                 if (val.indexOf("A") > -1) {
-                    arl_cnt += 1;
+                    $scope.arl_cnt += 1;
                 } else if (val.indexOf("B") > -1) {
-                    bts_cnt += 1;
+                    $scope.bts_cnt += 1;
                 } else if (val.indexOf("M") > -1) {
-                    mrt_cnt += 1;
+                    $scope.mrt_cnt += 1;
                 }
-
-                if (typeof data["route"][i+1] == "undefined") {
-                    return;
-                };
-                console.log(data["route"][i]);
-                if (data["route"][i+1] === "BCEN") {
-                    if (typeof data["route"][i+2] == "undefined") {
-                        return;
-                    };
-                    if (data["route"][i][1] == "N" && data["route"][i+2][1] == "E") {
-                        station_cnt[tmp] += 1;
-                    } else if (data["route"][i][1] == "E" && data["route"][i+2][1] == "N") {
-                        station_cnt[tmp] += 1;
-                    } else if (data["route"][i][1] == "W" && data["route"][i+2][1] == "S") {
-                        station_cnt[tmp] += 1;
-                    } else if (data["route"][i][1] == "S" && data["route"][i+2][1] == "W") {
-                        station_cnt[tmp] += 1;
-                    } else {
-                        station_cnt[tmp] += 1;
-                        station_cnt["BCEN"] = 0;
-                        tmp = data["route"][i+1];
-                        $scope.same_line = true;
-                    }
-                } else if (data["route"][i][0] === data["route"][i+1][0]) {
-                    station_cnt[tmp] += 1;
-                } else {
-                    station_cnt[data["route"][i+1]] = 0;
-                    tmp = data["route"][i+1];
-                }
-                console.log(station_cnt);
             });
-
-            $scope.station_cnt = station_cnt;
-            $scope.arl_cnt = arl_cnt;
-            $scope.bts_cnt = bts_cnt;
-            $scope.mrt_cnt = mrt_cnt;
-
         });
         res.error(function(data, status, headers, config) {
             $scope.response = JSON.stringify({data: data});
