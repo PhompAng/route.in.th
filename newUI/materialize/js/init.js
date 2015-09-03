a608a92b86d9a7485ec0ae4a5d9bd25717621efc
Nontawat Numor <nontawat39@gmail.com>
2015-09-04 02:22:02 +0700

    add newUI from @tonsai

---
+++ newUI/materialize/js/init.js
@@ -0,0 +1,9 @@
+$('#toggle_setting_modal').click(function() {
+	$('#setting_modal').openModal();
+});
+
+$(".button-collapse").sideNav();
+
+$(function() {
+    FastClick.attach(document.body);
+});
